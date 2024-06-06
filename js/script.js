1aa8892d67292f71a23a665eec34ac8050114f65
anntingg <162669802+anntingg@users.noreply.github.com>
2024-06-07 01:43:16 +0800

    0607-v1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// hamburger menu & top button
 $(function () {
     $(".nav-toggle").click(function () {
         $(".line").toggleClass("active");
@@ -17,4 +18,26 @@ $(function () {
     });
 })
 
+// banner
+/*
+window.addEventListener("scroll", () => {
+    const banner = document.querySelector("#banner-index");
+    const bannerImg = document.querySelector("#banner-index img");
+    const aboutSection = document.querySelector("#about");
 
+    let bannerHeight = banner.offsetHeight;
+    let aboutSectionTop = aboutSection.getBoundingClientRect().top;
+
+    let newWidth = 32 + (window.scrollY / 2);
+    newWidth = Math.min(newWidth, 100);
+
+    bannerImg.style.width = newWidth + 'vw';
+    bannerImg.style.height = newWidth + 'vw';
+
+    if (newWidth === 100 ) {
+        banner.classList.remove("fixed");
+    } else {
+        banner.classList.add("fixed");
+    }
+});
+*/
