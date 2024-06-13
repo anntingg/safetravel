fa0f727fb82a47fddd63fe7f26810fbe726dcd4c
anntingg <162669802+anntingg@users.noreply.github.com>
2024-06-13 16:37:08 +0800

    0613-v3

--- js/script-index.js
+++ js/script-index.js
@@ -35,37 +35,83 @@ window.addEventListener("resize", handleStep);
 
 
 // about section - parallax scrolling effect
+// window.addEventListener("scroll", () => {
+//     const aboutSection = document.querySelector("#about");
+//     const borderBox = document.querySelector(".border");
+//     const aboutSectionTop = aboutSection.getBoundingClientRect().top;
+//     const aboutSectionBottom = aboutSection.getBoundingClientRect().bottom;
+
+//     const intro1 = document.querySelector(".intro1");
+//     const intro2 = document.querySelector(".intro2");
+//     const intro3 = document.querySelector(".intro3");
+
+//     if (aboutSectionTop <= 0 && aboutSectionBottom >= window.innerHeight) {
+//         let translateValue1 = Math.max(aboutSectionTop, -250);
+//         let translateValue2 = Math.max(aboutSectionTop, -500);
+//         let scale1 = 1 - Math.abs(aboutSectionTop - 500) / window.innerHeight / 10;
+//         let scale2 = 1 - Math.abs(aboutSectionTop) / window.innerHeight / 10;
+
+//         borderBox.style.transform = `translateY(${-aboutSectionTop}px)`;
+//         intro1.style.transform = `scale(1)`;
+//         intro2.style.transform = `translateY(${translateValue1}px)`;
+//         intro3.style.transform = `translateY(${translateValue2}px)`;
+
+//         if (aboutSectionTop < -100) {
+//             intro1.style.transform = `scale(${scale1})`;
+//         }
+//         if (aboutSectionTop < -350) {
+//             intro2.style.transform = `translateY(${translateValue1}px) scale(${scale2})`;
+//         }
+//     }
+
+//     if (aboutSectionTop < (window.innerHeight / 2)) {
+//         borderBox.classList.add("active");
+//     }
+
+// });
+
+
+let isScrolling = false;
+
 window.addEventListener("scroll", () => {
-    const aboutSection = document.querySelector("#about");
-    const borderBox = document.querySelector(".border");
-    const aboutSectionTop = aboutSection.getBoundingClientRect().top;
-    const aboutSectionBottom = aboutSection.getBoundingClientRect().bottom;
-
-    const intro1 = document.querySelector(".intro1");
-    const intro2 = document.querySelector(".intro2");
-    const intro3 = document.querySelector(".intro3");
-
-    if (aboutSectionTop <= 0 && aboutSectionBottom >= window.innerHeight) {
-        let translateValue1 = Math.max(aboutSectionTop, -250);
-        let translateValue2 = Math.max(aboutSectionTop, -500);
-        let scale1 = 1 - Math.abs(aboutSectionTop -500) / window.innerHeight / 10;
-        let scale2 = 1 - Math.abs(aboutSectionTop) / window.innerHeight / 10;
-
-        borderBox.style.transform = `translateY(${-aboutSectionTop}px)`;
-        intro1.style.transform = `scale(1)`;
-        intro2.style.transform = `translateY(${translateValue1}px)`;
-        intro3.style.transform = `translateY(${translateValue2}px)`;
-
-        if (aboutSectionTop < -100) {
-            intro1.style.transform = `scale(${scale1})`;
-        }
-        if (aboutSectionTop < -350) {
-            intro2.style.transform = `translateY(${translateValue1}px) scale(${scale2})`;
-        }
-    }
-
-    if (aboutSectionTop < (window.innerHeight / 2)) {
-        borderBox.classList.add("active");
+    if (!isScrolling) {
+        window.requestAnimationFrame(() => {
+            const aboutSection = document.querySelector("#about");
+            const borderBox = document.querySelector(".border");
+            const aboutSectionTop = aboutSection.getBoundingClientRect().top;
+            const aboutSectionBottom = aboutSection.getBoundingClientRect().bottom;
+        
+            const intro1 = document.querySelector(".intro1");
+            const intro2 = document.querySelector(".intro2");
+            const intro3 = document.querySelector(".intro3");
+        
+            if (aboutSectionTop <= 0 && aboutSectionBottom >= window.innerHeight) {
+                let translateValue1 = Math.max(aboutSectionTop, -250);
+                let translateValue2 = Math.max(aboutSectionTop, -500);
+                let scale1 = 1 - Math.abs(aboutSectionTop - 500) / window.innerHeight / 10;
+                let scale2 = 1 - Math.abs(aboutSectionTop) / window.innerHeight / 10;
+        
+                borderBox.style.transform = `translateY(${-aboutSectionTop}px)`;
+                intro1.style.transform = `scale(1)`;
+                intro2.style.transform = `translateY(${translateValue1}px)`;
+                intro3.style.transform = `translateY(${translateValue2}px)`;
+        
+                if (aboutSectionTop < -100) {
+                    intro1.style.transform = `scale(${scale1})`;
+                }
+                if (aboutSectionTop < -350) {
+                    intro2.style.transform = `translateY(${translateValue1}px) scale(${scale2})`;
+                }
+            }
+        
+            if (aboutSectionTop < (window.innerHeight / 2)) {
+                borderBox.classList.add("active");
+            }
+        
+            isScrolling = false;
+        });
+        
+        isScrolling = true;
     }
 });
 
